9b80a5d76c8f67baea5f7009f741b60593d37770
Jean Carlos Ospino Gonzalez <devjc23@gmail.com>
2018-08-02 00:37:38 -0500

    Implementada la lectura del archivo en el servidor para mayor performance en el cliente

--- controllers/customers.js
+++ controllers/customers.js
@@ -1,32 +1,46 @@
 import { DB } from "../firebase"
 import moment from "moment"
+import papaparse from "papaparse"
+import fs from "fs"
 
-function writeMassive(req, res) {
-  const json = req.body.json
-  const customersRef = DB.ref("/customers")
+async function writeMassive(req, res) {
+  try {
+    const customersRef = DB.ref("/customers")
+    const c = await fs.readFileSync(req.files.csvFile.path, "utf8")
+    const d = papaparse.parse(c, {
+      complete: result => {
+        const now = moment().unix()
+        const selectedLocation = req.body.selectedLocation
+        const sourceKey = req.body.sourceKey
+        const uid = req.body.uid
 
-  json.forEach((value, index) => {
-    customersRef.child(value.placa).set({
-      createdAt: moment().unix(),
-      direccion: value.direccion,
-      fechaRtm: value.fechaRtm,
-      fechaRtmVencida: value.fechaRtmVencida,
-      fuenteKey: value.fuenteKey,
-      identificacion: value.identificacion,
-      linea: value.linea,
-      location: req.body.selectedLocation,
-      marca: value.marca,
-      modelo: value.modelo,
-      nombreCliente: value.nombreCliente,
-      placa: value.placa,
-      registro1: value.registro1,
-      registro2: value.registro2,
-      telefonos: value.telefonos,
-      tipoServicio: value.tipoServicio,
-      tipoVehiculo: value.tipoVehiculo
-    }).then(() => console.log(index, " customers registered succesfully!"))
-  })
-  res.status(200).json({ success: "PERFECT!!" })
+        result.data.forEach(x => {
+          DB.ref("/customers/" + x[2]).update({
+            fechaRtm: x[0],
+            nombreCliente: x[1],
+            registro1: x[3],
+            identificacion: x[4],
+            direccion: x[5],
+            telefonos: x[6],
+            marca: x[7],
+            linea: x[8],
+            tipoServicio: x[9],
+            tipoVehiculo: x[10],
+            modelo: x[11],
+            fechaRtmVencida: x[12],
+            registro2: x[13],
+            createdAt: now,
+            sourceKey: sourceKey,
+            location: selectedLocation,
+            createdBy: uid,
+          })
+        })
+        return res.status(200).json({ success: true })
+      }
+    })
+  } catch(ex) {
+    return res.status(500).json({ error: ex })
+  }
 }
 
 module.exports = { writeMassive }
--- routes/customers.js
+++ routes/customers.js
@@ -1,8 +1,9 @@
 import express from "express"
 import customersController from "../controllers/customers"
+import multipart from "connect-multiparty"
 
 const api = express.Router()
 
-api.post("/customers/write-massive", customersController.writeMassive)
+api.post("/customers/write-massive", multipart(), customersController.writeMassive)
 
 export default api
